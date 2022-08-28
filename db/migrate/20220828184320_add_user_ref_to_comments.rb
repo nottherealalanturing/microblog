9802800a0b77438164365fc5b3df9c85395da7f9
Assad Isah <nottherealalanturing@gmail.com>
2022-08-28 20:52:15 +0100

    chores: run updated migrations

--- db/migrate/20220828184320_add_user_ref_to_comments.rb
+++ db/migrate/20220828184320_add_user_ref_to_comments.rb
@@ -1,6 +1,6 @@
 class AddUserRefToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :author_id, foreign_key: { to_table: :users}
-    add_reference :comments, :post_id
+    add_reference :comments, :author, foreign_key: { to_table: :users}
+    add_reference :comments, :post
   end
 end
