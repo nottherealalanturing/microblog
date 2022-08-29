2ebaa862bcd7e73bcbedca556dfd1da4a7dd8c1a
Assad Isah <nottherealalanturing@gmail.com>
2022-08-29 02:22:46 +0100

    fix: resolve linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,13 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-  validates :title, presence: true, length: { maximum: 250}
-  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0}
-
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
   private
 
   def posts_count
-    author.update(posts_counter: author.posts.count)    
-
+    author.update(posts_counter: author.posts.count)
   end
 
   def posts_count
